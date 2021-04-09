f5049e2d4ce692d634ffacade4f5e0c01822e214
libertussabebeget <82089052+libertussabebeget@users.noreply.github.com>
2021-04-09 20:04:59 +0700

    Menghapus baris code
    Menghapus baris code line 8 sam 9

--- MODULE.vb
+++ MODULE.vb
@@ -5,8 +5,6 @@ Module Module1
     Public Rd As SqlDataReader
     Public Cmd As SqlCommand
     Public Ds As DataSet
-    Public SimpanData, UbahData, HapusData As String
-    Public SlideMenu As String = "Close"
 
     Public Sub Koneksi()
         Conn = New SqlConnection("Data Source=LIBERTUS; initial catalog=DB_BahanAjar; integrated security=true")
