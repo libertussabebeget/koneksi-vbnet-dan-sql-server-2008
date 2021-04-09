47f9a3fbae0ca62007611659178b70e37d883b2c
libertussabebeget <82089052+libertussabebeget@users.noreply.github.com>
2021-04-09 20:01:38 +0700

    Menambahkan File Koneksi vbnet

---
+++ MODULE.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+Module Module1
+    Public Conn As SqlConnection
+    Public Da As SqlDataAdapter
+    Public Rd As SqlDataReader
+    Public Cmd As SqlCommand
+    Public Ds As DataSet
+    Public SimpanData, UbahData, HapusData As String
+    Public SlideMenu As String = "Close"
+
+    Public Sub Koneksi()
+        Conn = New SqlConnection("Data Source=LIBERTUS; initial catalog=DB_BahanAjar; integrated security=true")
+        Conn.Open()
+    End Sub
+End Module
